50fbf7135b9037d8032233bdd89e3e4ce971daca
Iván Cortés Aliende <ivancortes4a@gmail.com>
2023-04-12 20:36:23 +0200

    Edit and delete options implemented

--- src/main.py
+++ src/main.py
@@ -27,22 +27,6 @@ def launch_error(code:int, details:str):
     html_code = html_code.replace(f'%%_ERROR_DETAILS_%%', details)
     return HTMLResponse(content=html_code, status_code=code)
 
-def add_redirect(domain: str, query_id: str, url: str, admin_key: str):
-    entry = search_url(domain, query_id)
-    if type(entry) != Entry:
-        if admin_key == ADMIN_KEY:
-            entry_dict = {'query_id': query_id, "url": url, "domain": domain}
-            db_client.redirections.insert_one(entry_dict)
-            inserted_entry = search_url(domain, query_id)
-            if type(inserted_entry) != Entry:
-                print(type(inserted_entry))
-                return launch_error(400, "Error creating the entry")
-            else:
-                return "Entry created succesfully"
-        else:
-             return launch_error(400, "Not valid admin_key")
-    return launch_error(404, "Redirection already exists")
-
 @app.get('/')
 async def index():
     html_code = Path('html/index.html').read_text()
@@ -59,10 +43,46 @@ async def redirect(query_id: str, request: Request):
     html_code = html_code.replace(f'%%_URL_%%', entry.url)
     return HTMLResponse(content=html_code, status_code=200)
 
-@app.post('/json')
+@app.post('/')
 async def post_redirect(body: Entry):
-    return add_redirect(domain=body.domain, query_id=body.query_id, url=body.url, admin_key=body.admin_key)
+    entry = search_url(body.domain, body.query_id)
+    if type(entry) != Entry:
+        if body.admin_key == ADMIN_KEY:
+            entry_dict = {'query_id': body.query_id, "url": body.url, "domain": body.domain}
+            db_client.redirections.insert_one(entry_dict)
+            inserted_entry = search_url(body.domain, body.query_id)
+            if type(inserted_entry) != Entry:
+                return launch_error(400, "Error creating the entry")
+            else:
+                return "Entry created succesfully"
+        else:
+             return launch_error(400, "Not valid admin_key")
+    return launch_error(404, "Redirection already exists")
 
-@app.post('/form')
-async def post_redirect(domain:str = Form(...), query_id:str = Form(...), url:str = Form(...), admin_key:str = Form(...)):
-    return add_redirect(domain=domain, query_id=query_id, url=url, admin_key=admin_key)
+@app.delete('/{domain}/{query_id}')
+async def delete_redirect(domain: str, query_id: str, admin_key: str):
+    entry = search_url(domain, query_id)
+    if type(entry) == Entry:
+        if admin_key == ADMIN_KEY:
+            db_client.redirections.find_one_and_delete({"domain": domain, "query_id": query_id})
+            return "Entry deleted succesfully"
+        else:
+             return launch_error(400, "Not valid admin_key")
+    else:
+         return launch_error(404, "Redirection not found")
+
+@app.put('/')
+async def put_redirect(body: Entry):
+    entry = search_url(body.domain, body.query_id)
+    if type(entry) == Entry:
+        if body.admin_key == ADMIN_KEY:
+            entry_dict = {'query_id': body.query_id, "url": body.url, "domain": body.domain}
+            db_client.redirections.find_one_and_replace({"domain": body.domain, "query_id": body.query_id}, entry_dict)
+            edited_entry = search_url(body.domain, body.query_id)
+            if edited_entry.url != body.url:
+                return launch_error(400, "Error modifying the entry")
+            else:
+                return "Entry modified succesfully"
+        else:
+             return launch_error(400, "Not valid admin_key")
+    return launch_error(404, "Redirection not found")
